01cbd5bae0247ffe9adafed3a283475df33a677a
xwang875 <xwang875@gatech.edu>
2018-05-08 21:00:23 -0400

    Singleton pattern

---
+++ Singleton/Singleton.java
@@ -0,0 +1,11 @@
+package Singleton;
+
+public final class Singleton {
+    private static final Singleton INSTANCE = new Singleton();
+
+    private Singleton(){}
+
+    public static Singleton getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ Singleton/SingletonLazy.java
@@ -0,0 +1,18 @@
+package Singleton;
+
+public final class SingletonLazy {
+    private static volatile SingletonLazy instance = null;
+
+    private SingletonLazy(){}
+
+    public static SingletonLazy getInstance() {
+        if (instance == null) {
+            synchronized(SingletonLazy.class) {
+                if (instance == null) {
+                    instance = new SingletonLazy();
+                }
+            }
+        }
+        return instance;
+    }
+}
