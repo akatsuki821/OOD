9b17a737b39dc9b2ab6db3b1d9e15d7a2da90e51
xwang875 <xwang875@gatech.edu>
2018-05-09 12:12:44 -0400

    black jack game simulator

---
+++ BlackJack/Hand.java
@@ -0,0 +1,29 @@
+package BlackJack;
+
+import java.util.*;
+
+public class Hand {
+    protected final List<Card> cards = new ArrayList<>();
+
+    public int score() {
+        int score = 0;
+        for (Card card : cards) {
+            score += card.value();
+        }
+        return score;
+    }
+
+    public void addCards(Card[] c) {
+        Collections.addAll(cards, c);
+    }
+
+    public int size() {
+        return cards.size();
+    }
+
+    public void print() {
+        for (Card c : cards) {
+            System.out.println(c.suit() + " " + c.value());
+        }
+    }
+}
